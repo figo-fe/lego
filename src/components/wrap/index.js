3810a78dc076c37786f451eb4f5fe8c197addf9b
figo <23335628@qq.com>
2019-10-29 13:37:04 +0800

    嵌入式显示 fix

--- src/components/wrap/index.js
+++ src/components/wrap/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SettingContext } from '../../config/context';
 
 export const Wrap = ({ children, title = '' }) => {
+  const { mode } = useContext(SettingContext);
+
   return (
     <div className='wrap'>
       {title.length > 0 && <div className='sitepath'>{title}</div>}
-      <div className='main-content'>{children}</div>
+      <div className={'main-content' + (mode === 'embedded' ? ' main-content-embedded' : '')}>{children}</div>
     </div>
   );
 };
