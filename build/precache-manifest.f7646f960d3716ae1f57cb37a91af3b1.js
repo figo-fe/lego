a12d19d150985a375c4564e5dda9b165b3f705ee
figo <23335628@qq.com>
2019-12-11 17:19:12 +0800

    提交build目录

---
+++ build/precache-manifest.f7646f960d3716ae1f57cb37a91af3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e0f4a8d4cbc94e6d0a7cea44843235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25079dd528f0160c1cf9",
+    "url": "/static/css/2.7fe71732.chunk.css"
+  },
+  {
+    "revision": "e09651f8c767b7a0c7ed",
+    "url": "/static/css/main.b4edeef0.chunk.css"
+  },
+  {
+    "revision": "25079dd528f0160c1cf9",
+    "url": "/static/js/2.c1a254d9.chunk.js"
+  },
+  {
+    "revision": "e09651f8c767b7a0c7ed",
+    "url": "/static/js/main.bf636a01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
